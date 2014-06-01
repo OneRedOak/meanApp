7df293ffad91018ac3fdcb43fad7faf7666985e5
Patrick Ellis <oneredoak@gmail.com>
2014-06-01 00:55:36 -0700

    MongoDB & Mongoose Added to Stack & Configured

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express'),
-    stylus = require('stylus');
+    stylus = require('stylus'),
+    mongoose = require('mongoose');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || "development";
 var app = express();
@@ -7,7 +8,6 @@ var app = express();
 function compile(str, path) {
     return stylus(str).set('filename', path);
 }
-
 
 app.configure(function() {
     app.set('views', __dirname + '/server/views');
@@ -23,12 +23,28 @@ app.configure(function() {
     app.use(express.static(__dirname + '/public'));
 });
 
+mongoose.connect('mongodb://localhost/meanapp');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'conneciton error...'));
+db.once('open', function callback() {
+    console.log('meanapp db opended');
+});
+
+var messageSchema = mongoose.Schema({message: String});
+var Message = mongoose.model('Message', messageSchema);
+var mongoMessage;
+Message.findOne().exec(function(err, messageDoc) {
+    mongoMessage = messageDoc.message;
+});
+
 app.get('/partials/:partialPath', function(req, res) {
     res.render('partials/' + req.params.partialPath);
 });
 
 app.get('*', function(req, res) {
-    res.render('index');
+    res.render('index', {
+        mongoMessage: mongoMessage
+    });
 });
 
 var port = 3030;
